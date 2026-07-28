06172ebcd41dc000f88c7ce36f5f2a5eeee6389e
jineasaha <jinea05saha@gmail.com>
2026-07-28 23:41:27 +0530

    added student features in landing page

--- src/app/login/LoginForm.jsx
+++ src/app/login/LoginForm.jsx
@@ -1,5 +1,4 @@
 import { useState, useContext } from "react";
-import { Link } from "react-router-dom";
 import AlertContext from "../../context/alert/AlertContext";
 import "./Auth.css";
 
--- src/app/login/SignupForm.jsx
+++ src/app/login/SignupForm.jsx
@@ -1,5 +1,5 @@
 import { useState, useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import AlertContext from "../../context/alert/AlertContext";
 import "./Auth.css";
 
